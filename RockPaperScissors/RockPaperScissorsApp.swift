36eae951505174cc1a9e1d1ef34e1b94f093af21
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-06-27 18:16:24 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Bruce Gilmour on 2021-06-27.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
